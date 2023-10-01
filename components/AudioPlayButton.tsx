ea82c08e792b5dfac94c3a1ae2e4401bbaafe1b5
chenjunqian <chenjunqian0810@foxmail.com>
2023-10-02 00:59:55 +0800

    feat: add player button component

---
+++ components/AudioPlayButton.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useAudioPlayer } from './AudioPlayerContext'
+
+export default function AudioPlayButton({ data }: { data: AudioPlayerParams }) {
+
+    const { updateAudio, play, pause } = useAudioPlayer()
+
+    const parseHtmlStrinText = (htmlString: string): string => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlString, 'text/html');
+        const textContent = doc.body.textContent;
+        return textContent ?? '';
+    }
+
+    const onPlayBtnClick = () => {
+        console.log("play button clicked")
+        var textTitle = parseHtmlStrinText(data.title)
+        var textAuthorName = parseHtmlStrinText(data.artist)
+
+        const playerParams: AudioPlayerParams = {
+            title: textTitle,
+            artist: textAuthorName,
+            cover: data.cover,
+            src: data.src
+        }
+        updateAudio(playerParams)
+        play()
+    }
+
+    return (
+        <>
+            <button className="btn btn-circle btn-outline btn-sm" onClick={onPlayBtnClick}>
+                <svg className="fill-current rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8,5.14V19.14L19,12.14L8,5.14Z" /></svg>
+            </button>
+        </>
+    )
+}
