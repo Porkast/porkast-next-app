79042fc25fb9e79ba785e55d9356295ea1c871b7
chenjunqian <chenjunqian0810@foxmail.com>
2023-10-02 01:26:35 +0800

    feat: add channel and item types

---
+++ types/feed_item.ts
@@ -0,0 +1,28 @@
+type FeedItem = {
+    Id: string;
+    ChannelId: string;
+    Title: string;
+    HighlightTitle: string;
+    Link: string;
+    PubDate: string;
+    Author: string;
+    InputDate: Date;
+    ImageUrl: string;
+    EnclosureUrl: string;
+    EnclosureType: string;
+    EnclosureLength: string;
+    Duration: string;
+    Episode: string;
+    Explicit: string;
+    Season: string;
+    EpisodeType: string;
+    Description: string;
+    TextDescription: string;
+    ChannelImageUrl: string;
+    ChannelTitle: string;
+    HighlightChannelTitle: string;
+    FeedLink: string;
+    Count: number;
+    TookTime: number;
+    HasThumbnail: boolean;
+}
