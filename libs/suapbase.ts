1dab1c00d1df2b572a91226703bc2c637da305ad
chenjunqian <chenjunqian0810@foxmail.com>
2023-10-06 13:58:25 +0800

    feat: check is login

--- libs/suapbase.ts
+++ libs/suapbase.ts
@@ -1,8 +1,17 @@
 
-import { createClient } from '@supabase/supabase-js'
+import { Database } from '@/types/supabase'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
-const supabaseUrl = 'https://qojpygjnxacuxdrtzdtz.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey || '')
+// const supabaseUrl = 'https://qojpygjnxacuxdrtzdtz.supabase.co'
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabase = createClientComponentClient<Database>()
+
+
+export const isUserLoggedIn = async (): Promise<boolean> => {
+    const { data: { session }, } = await supabase.auth.getSession()
+    return session ? true : false
+}
+
+
 
 export default supabase
