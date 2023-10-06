38931a4eb4585af0d638f8db5374292ac35a96ef
chenjunqian <chenjunqian0810@foxmail.com>
2023-10-07 00:07:50 +0800

    fix: adjust style for mobile

--- app/podcast/[channelId]/episode/[itemId]/page.tsx
+++ app/podcast/[channelId]/episode/[itemId]/page.tsx
@@ -5,6 +5,8 @@ import { cache } from "react"
 import parse from 'html-react-parser'
 import AudioPlayButton from "@/components/AudioPlayButton"
 import { AppProvider } from "@/components/AppContext"
+import AddListenLaterButton from "@/components/AddListenLaterButton"
+import AddToPlaylistButton from "@/components/AddToPlaylistButton"
 
 
 export default async function Page({ params }: { params: { channelId: string, itemId: string } }) {
@@ -40,14 +42,14 @@ export default async function Page({ params }: { params: { channelId: string, it
                                 <a href={podcastChannelLink} className="text-base font-bold mt-2">{channelInfo.Title}</a>
                                 <div className="text-sm font-medium text-gray-500 mt-2">By {channelInfo.Author}</div>
                                 <div className="flex justify-start mt-4">
-                                    <a className="btn btn-neutral btn-sm flex items-center rounded-lg" href={itemInfo.FeedLink} target="_blank">
-                                        <svg className="w-4 h-4 icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4715" width="32" height="32">
+                                    <a className="btn btn-neutral btn-sm flex items-center rounded-lg md:text-base text-xs" href={itemInfo.FeedLink} target="_blank">
+                                        <svg className="md:w-4 md:h-4 w-3 h-3 icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4715" width="32" height="32">
                                             <path d="M128 768a128 128 0 1 0 0 256 128 128 0 0 0 0-256zM0 368v176c265.104 0 480 214.912 480 480h176c0-362.32-293.696-656-656-656zM0 0v176c468.336 0 848 379.664 848 848h176C1024 458.464 565.536 0 0 0z" fill="#bfbfbf" p-id="4716"></path>
                                         </svg>
                                         RSS
                                     </a>
-                                    <a className="btn btn-neutral btn-sm flex items-center rounded-lg ml-4" href={itemInfo.Link} target="_blank">
-                                        <svg className="w-5 h-5 icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3342" width="32" height="32"><path d="M574 665.4c-3.1-3.1-8.2-3.1-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8c-3.1-3.1-8.2-3.1-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zM832.6 191.4c-84.6-84.6-221.5-84.6-306 0L410.3 307.6c-3.1 3.1-3.1 8.2 0 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6c-3.1 3.1-3.1 8.2 0 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1z" p-id="3343" fill="#bfbfbf"></path><path d="M610.1 372.3c-3.1-3.1-8.2-3.1-11.3 0L372.3 598.7c-3.1 3.1-3.1 8.2 0 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z" p-id="3344" fill="#bfbfbf"></path></svg>
+                                    <a className="btn btn-neutral btn-sm flex items-center rounded-lg ml-4 md:text-base text-xs" href={itemInfo.Link} target="_blank">
+                                        <svg className="md:w-5 md:h-5 w-4 h-4 icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3342" width="32" height="32"><path d="M574 665.4c-3.1-3.1-8.2-3.1-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8c-3.1-3.1-8.2-3.1-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zM832.6 191.4c-84.6-84.6-221.5-84.6-306 0L410.3 307.6c-3.1 3.1-3.1 8.2 0 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6c-3.1 3.1-3.1 8.2 0 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1z" p-id="3343" fill="#bfbfbf"></path><path d="M610.1 372.3c-3.1-3.1-8.2-3.1-11.3 0L372.3 598.7c-3.1 3.1-3.1 8.2 0 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z" p-id="3344" fill="#bfbfbf"></path></svg>
                                         Source Link
                                     </a>
                                 </div>
@@ -58,17 +60,8 @@ export default async function Page({ params }: { params: { channelId: string, it
                             {/* play icon */}
                             <AudioPlayButton data={playerParams} />
                             <div className="text-base text-gray-500 w-20 ml-4">{itemInfo.Duration}</div>
-                            <button className="btn btn-neutral btn-sm flex items-center rounded-lg">
-                                <svg className="w-4 h-4" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z" />
-                                    <path fill="currentColor" d="M13 7h-2v6l4.2 2.5.8-1.2-3.5-2.1z" />
-                                </svg>
-                                <span className="font-bold text-xs md:display">Listen Later</span>
-                            </button>
-                            <button className="btn btn-neutral ml-2 btn-sm flex items-center rounded-lg">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4"><path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /><path fill="none" d="M0 0h24v24H0z" /></svg>
-                                <span className="font-bold text-xs md:display">Add</span>
-                            </button>
+                            <AddListenLaterButton userId={''} itemId={data.itemId} channelId={data.channelId} />
+                            <AddToPlaylistButton userId={''} itemId={data.itemId} channelId={data.channelId} playlistId={''} />
                         </div>
                         <p className="mt-4 text-xs">{channelInfo.Copyright}</p>
                         <div className="card w-full bg-base-100 shadow-xl overflow-auto mt-9">
