87a6bb0a30d1e9ba89ba8dcd022195b50fa6fdb7
chenjunqian <chenjunqian0810@foxmail.com>
2023-10-11 01:50:50 +0800

    feat: add alert

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
+import { MsgAlert, MsgAlertRef, MsgAlertType } from "@/components/MsgAlert"
 import { isUserLoggedIn, updatePassword, userSignout } from "@/libs/suapbase"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function ResetPasswordPage() {
 
     const router = useRouter()
+    const msgAlertRef = useRef<MsgAlertRef>(null)
     const [isValidResetPasswordLink, setIsValidResetPasswordLink] = useState(false)
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
@@ -43,60 +45,61 @@ export default function ResetPasswordPage() {
 
     const doResetPassword = async () => {
         if (!checkPassword(password)) {
-            // TODO: show error
-            alert('Password must at least 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, and 1 number.')
+            msgAlertRef.current?.showAlert('Password must at least 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, and 1 number.', MsgAlertType.FAILED)
             return
         } else if (password !== confirmPassword) {
-            // TODO: show error
-            alert('Passwords do not match.')
+            msgAlertRef.current?.showAlert('Passwords do not match.', MsgAlertType.FAILED)
             return
         }
         const resp = await updatePassword(password)
         if (resp.code === 0) {
             // update password successfully, sign out and redirect to login page 
+            msgAlertRef.current?.showAlert('Password updated successfully', MsgAlertType.SUCCESS)
             await userSignout()
             router.push('/signin')
         } else {
             // update password failed
-            // TODO: show error
-            alert(resp.message)
+            msgAlertRef.current?.showAlert(resp.message, MsgAlertType.FAILED)
         }
     }
 
     return (
-        <div className="w-full min-h-screen flex justify-center">
-            {
-                isValidResetPasswordLink ?
-                    (
-                        <div className="w-full max-w-md pt-32 pl-6 pr-6">
-                            <div className="w-full flex justify-center text-xl">
-                                Please reset your password.
+        <>
+            <div className="w-full min-h-screen flex justify-center">
+                {
+                    isValidResetPasswordLink ?
+                        (
+                            <div className="w-full max-w-md pt-32 pl-6 pr-6">
+                                <div className="w-full flex justify-center text-xl">
+                                    Please reset your password.
+                                </div>
+                                <div className="form-control w-full mt-9">
+                                    <label className="label">
+                                        <span className="label-text font-bold accent-content">Password</span>
+                                    </label>
+                                    <input onChange={onPasswordChanged} type="password" placeholder="Input password" className="input input-bordered w-full accent-content" />
+                                </div>
+                                <div className="text-neutral-content mt-4">
+                                    Password must at least 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, and 1 number.
+                                </div>
+                                <div className="form-control w-full mt-4">
+                                    <label className="label">
+                                        <span className="label-text font-bold">Confirm Password</span>
+                                    </label>
+                                    <input onChange={onConfirmPasswordChanged} type="password" placeholder="Repeat password" className="input input-bordered w-full accent-content" />
+                                </div>
+                                <button onClick={() => doResetPassword()} className="btn w-full mt-9">Submit</button>
                             </div>
-                            <div className="form-control w-full mt-9">
-                                <label className="label">
-                                    <span className="label-text font-bold accent-content">Password</span>
-                                </label>
-                                <input onChange={onPasswordChanged} type="password" placeholder="Input password" className="input input-bordered w-full accent-content" />
+                        ) : (
+                            <div className="w-full max-w-md pt-32 pl-6 pr-6">
+                                <div className="w-full flex justify-center text-xl">
+                                    Please Login First
+                                </div>
                             </div>
-                            <div className="text-neutral-content mt-4">
-                                Password must at least 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, and 1 number.
-                            </div>
-                            <div className="form-control w-full mt-4">
-                                <label className="label">
-                                    <span className="label-text font-bold">Confirm Password</span>
-                                </label>
-                                <input onChange={onConfirmPasswordChanged} type="password" placeholder="Repeat password" className="input input-bordered w-full accent-content" />
-                            </div>
-                            <button onClick={() => doResetPassword()} className="btn w-full mt-9">Submit</button>
-                        </div>
-                    ) : (
-                        <div className="w-full max-w-md pt-32 pl-6 pr-6">
-                            <div className="w-full flex justify-center text-xl">
-                                Invalid Reset Password Link
-                            </div>
-                        </div>
-                    )
-            }
-        </div>
+                        )
+                }
+            </div>
+            <MsgAlert ref={msgAlertRef} />
+        </>
     )
 }
