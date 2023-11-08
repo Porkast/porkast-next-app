29e5a985a6344dc5f908f7631dbeda5dd49a01b9
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-09 00:33:05 +0800

    feat: add warn alert

--- components/MsgAlert.tsx
+++ components/MsgAlert.tsx
@@ -5,7 +5,8 @@ import { Ref, forwardRef, useEffect, useState } from "react";
 
 export enum MsgAlertType {
     SUCCESS,
-    FAILED
+    WARN,
+    FAILED,
 }
 
 export type MsgAlertRef = {
@@ -37,14 +38,20 @@ export const MsgAlert = forwardRef<MsgAlertRef, MsgAlertProps>((props, ref: Ref<
         }
     }, [])
 
+    const renderMsgAlert = () => {
+        if (msgType === MsgAlertType.SUCCESS) {
+            return <SuccessMsgAlert msg={msg} />
+        } else if (msgType === MsgAlertType.WARN) {
+            return <WarningMsgAlert msg={msg} />
+        } else if (msgType === MsgAlertType.FAILED) {
+            return <FailedMsgAlert msg={msg} />
+        }
+        return <></>
+    }
+
     return (
         <div className=" fixed top-0 left-0 right-0 z-50" style={{ visibility: show ? "visible" : "hidden" }}>
-            {
-                msgType === MsgAlertType.SUCCESS ?
-                    <SuccessMsgAlert msg={msg} />
-                    :
-                    <FailedMsgAlert msg={msg} />
-            }
+            {renderMsgAlert()}
         </div>
     )
 })
@@ -70,5 +77,14 @@ const FailedMsgAlert = ({ msg }: { msg: string }) => {
                 <span>{msg}</span>
             </div>
         </>
+    )
+}
+
+const WarningMsgAlert = ({ msg }: { msg: string }) => {
+    return (
+        <div className="alert alert-warning">
+            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+            <span>{msg}</span>
+        </div>
     )
 }
