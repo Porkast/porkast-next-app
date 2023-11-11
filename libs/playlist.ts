5c84da89bcd739d97c0d3f3cf177b585314f1580
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-11 20:54:16 +0800

    feat: get user playlist list api

--- libs/playlist.ts
+++ libs/playlist.ts
@@ -18,3 +18,16 @@ export async function addToPlayList(channelId: string, itemId: string, playlistI
 
     return respJson;
 }
+
+export async function getUserPlaylistByUserId(userId: string): Promise<JsonResponse> {
+
+    const respJson = await fetch(`${process.env.API_BASE_URL}v1/api/playlist/list/${userId}`).then(resp => resp.json()).catch(err => {
+        console.log(err);
+    })
+
+    return {
+        code: respJson.code,
+        message: respJson.message,
+        data: respJson.data
+    }
+}
