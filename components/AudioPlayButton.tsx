0dcf8f60630634b5a718e1994d102ef544ed4ce2
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-21 21:10:09 +0800

    fix: adjust for theme

--- components/AudioPlayButton.tsx
+++ components/AudioPlayButton.tsx
@@ -14,7 +14,6 @@ export default function AudioPlayButton({ data }: { data: AudioPlayerParams }) {
     }
 
     const onPlayBtnClick = () => {
-        console.log("play button clicked")
         var textTitle = parseHtmlStrinText(data.title)
         var textAuthorName = parseHtmlStrinText(data.artist)
 
@@ -31,7 +30,8 @@ export default function AudioPlayButton({ data }: { data: AudioPlayerParams }) {
     return (
         <>
             <button className="btn btn-circle btn-outline btn-sm" onClick={onPlayBtnClick}>
-                <svg className="fill-current rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8,5.14V19.14L19,12.14L8,5.14Z" /></svg>
+                {/* <svg className="fill-current rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8,5.14V19.14L19,12.14L8,5.14Z" /></svg> */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="fill-current rounded-full ml-1"><polygon points="5 3 19 12 5 21 5 3" /></svg>
             </button>
         </>
     )
