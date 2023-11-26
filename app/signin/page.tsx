b885d17c0dcf284b6f561572829ec14d2cb09bf8
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-26 18:47:13 +0800

    style: set to dark theme

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -30,14 +30,23 @@ export default function AuthForm() {
         <>
             <Header hideSearchBtn={true} >
                 <div className="w-full flex justify-center min-h-screen mt-20">
-                    <div data-theme="light" className="w-full max-w-md pt-32 pl-6 pr-6">
+                    <div className="w-full max-w-md pt-32 pl-6 pr-6">
                         <Auth
+                            theme="dark"
                             supabaseClient={supabase}
                             magicLink={true}
                             appearance={{
                                 theme: ThemeSupa,
                                 className: {
                                     input: 'text-accent-content',
+                                },
+                                variables: {
+                                    default: {
+                                        colors: {
+                                            brand: '#404040',
+                                            brandAccent: '#52525b'
+                                        }
+                                    }
                                 }
                             }}
                             providers={['google']}
