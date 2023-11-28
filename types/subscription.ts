8ae9e9a1b46e1f7a715904194f0b64309f768294
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-29 01:39:42 +0800

    feat: do search subscription

---
+++ types/subscription.ts
@@ -0,0 +1,11 @@
+
+type KeywordSubscription = {
+    Id?: number;
+    Keyword: string;
+    FeedChannelId: string;
+    FeedItemId: string;
+    CreateTime: Date;
+    Country: string;
+    Source: string;
+    ExcludeFeedId: string;
+};
