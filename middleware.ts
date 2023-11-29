bd361e6547861265a0e73823fe5f39af663ddd16
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-30 01:46:27 +0800

    feat: add user listen later list GET api

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { verifyAuth } from './libs/auth'
 
-const authAPIRoutes = ['/api/subscription/keyword', '/api/user/sync']
+const authAPIRoutes = ['/api/subscription/keyword', '/api/user/sync', '/api/listenlater/item']
 
 export async function middleware(req: NextRequest) {
     const res = NextResponse.next()
