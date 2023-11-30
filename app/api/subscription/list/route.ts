0cc9716ba7d5a228ab7b1ce4a7483a1ec8304689
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-30 22:09:47 +0800

    feat: add user keyword subscription list API

---
+++ app/api/subscription/list/route.ts
@@ -0,0 +1,29 @@
+import { queryUserKeywordSubscriptionList } from "@/libs/db/subscription";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(request: NextRequest) {
+
+    const resp: JsonResponse = {
+        code: 0,
+        message: '',
+        data: null
+    }
+    const searchParams = request.nextUrl.searchParams
+    const userId = searchParams.get('userId')
+    const limit = searchParams.get('limit') || 10
+    const offset = searchParams.get('offset') || 0
+
+    if (!userId) {
+        resp.code = 1
+        resp.message = 'User ID is required'
+        return NextResponse.json(resp)
+    }
+
+    let queryListData: KeywordSubscription[] = await queryUserKeywordSubscriptionList(userId, Number(limit), Number(offset))
+
+    resp.code = 0
+    resp.message = 'OK'
+    resp.data = queryListData
+    return NextResponse.json(resp)
+}
