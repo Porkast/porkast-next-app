5b05427f7893d69ccf7512237332b6d882ac1bf6
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-30 21:54:07 +0800

    feat: playlist api

--- app/api/playlist/list/[userId]/route.ts
+++ app/api/playlist/list/[userId]/route.ts
@@ -1,6 +1,44 @@
-import { NextRequest } from "next/server";
+import { queryUserPlaylistListByUserId } from "@/libs/db/playlist";
+import prisma from "@/libs/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { useId } from "react";
 
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest, { params }: { params: { userId: string } }) {
 
+    const userId = params.userId
+    const searchParams = request.nextUrl.searchParams
+    const offset = Number(searchParams.get('offset')) || 0
+    const limit = Number(searchParams.get('limit')) || 10
+    const resp: JsonResponse = {
+        code: 0,
+        message: '',
+        data: null
+    }
+
+    if (!useId) {
+        resp.code = 1
+        resp.message = 'Missing required fields'
+        return NextResponse.json(resp)
+    }
+
+    const playlistList = await prisma.user_playlist.findMany({
+        where: {
+            user_id: userId
+        }
+    })
+
+    if (!playlistList) {
+        resp.code = 1
+        resp.message = 'Playlist not found'
+        return NextResponse.json(resp)
+    }
+
+    const resultDtos = await queryUserPlaylistListByUserId(userId, offset, limit)
+
+    resp.code = 0
+    resp.message = 'Ok'
+    resp.data = resultDtos
+
+    return NextResponse.json(resp)
 }
--- types/playlist.ts
+++ types/playlist.ts
@@ -12,3 +12,40 @@ export type UserPlaylistDto = {
     Count: number;
     UserInfo?: ServerUserInfo
 }
+
+export type UserPlaylistItemDto = {
+    Id: string;
+    FeedId: string;
+    GUID: string;
+    ChannelId: string;
+    Title: string;
+    HighlightTitle: string;
+    Link: string;
+    PubDate: string;
+    Author: string;
+    InputDate: string;
+    ImageUrl: string;
+    EnclosureUrl: string;
+    EnclosureType: string;
+    EnclosureLength: string;
+    Duration: string;
+    Episode: string;
+    Explicit: string;
+    Season: string;
+    EpisodeType: string;
+    Description: string;
+    TextDescription: string;
+    ChannelImageUrl: string;
+    ChannelTitle: string;
+    HighlightChannelTitle: string;
+    FeedLink: string;
+    Count: number;
+    Source: string
+    ExcludeFeedId: string
+    Country: string
+    TookTime: number;
+    HasThumbnail: boolean;
+    RegDate: string;
+    Status: number;
+    PlaylistId: string;
+}
