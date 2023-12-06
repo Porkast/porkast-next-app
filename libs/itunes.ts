1e22bf4c989a1529d80997ad8055072fad6802fa
chenjunqian <chenjunqian0810@foxmail.com>
2023-12-06 21:40:59 +0800

    feat: no cache for itunes api

--- libs/itunes.ts
+++ libs/itunes.ts
@@ -5,7 +5,7 @@ import xml2js from 'xml2js';
 import { RSS } from "@/types/feed";
 
 export const searchPodcastEpisodeFromItunes = async (q: string, entity: string, country: string, excludeFeedId: string, offset: number, limit: number, totalCount: number): Promise<FeedItem[]> => {
-    const res = await fetch(`https://itunes.apple.com/search?term=${q}&entity=${entity}&media=podcast&country=${country}&limit=${totalCount}`)
+    const res = await fetch(`https://itunes.apple.com/search?term=${q}&entity=${entity}&media=podcast&country=${country}&limit=${totalCount}`, { cache: 'no-store' })
     const jsonResp = await res.json()
     var items: FeedItem[] = []
     const excludeFeedIdList = excludeFeedId.split(',')
@@ -119,7 +119,7 @@ export const getPodcastAllInfo = async (podcastId: string): Promise<{ podcast: F
 }
 
 export const getPodcastEpisodeInfo = async (podcastId: string, episodeId: string): Promise<{ podcast: FeedChannel, episode: FeedItem }> => {
-    const res = await fetch(`https://itunes.apple.com/lookup?id=${podcastId}&entity=podcast`)
+    const res = await fetch(`https://itunes.apple.com/lookup?id=${podcastId}&entity=podcast`, { cache: 'no-store' })
     const jsonResp = await res.json()
     const podcastInfo = jsonResp.results[0]
     const feedLink = podcastInfo.feedUrl
