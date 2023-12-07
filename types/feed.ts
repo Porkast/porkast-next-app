5de0b9ef11050369ae7538dd3ac0f202d05249b5
chenjunqian <chenjunqian0810@foxmail.com>
2023-12-07 22:46:28 +0800

    fix: add podcast feed model

--- types/feed.ts
+++ types/feed.ts
@@ -1,3 +1,5 @@
+import { Feed, Item } from "podcast";
+
 export interface RSS {
     version: string;
     encoding: string;
@@ -60,4 +62,33 @@ export interface RSS {
             }[];
         };
     };
+}
+
+
+export interface PodcastFeed extends Feed {
+    itunes: {
+        title: string
+        author: string
+        summary: string
+        owner: {
+            name: string
+            email: string
+        }
+        image: string
+        type: string
+        categories: string[]
+    }
+    items: PodcastItem
+}
+
+export interface PodcastItem extends Item {
+    itunes: {
+        duration: string
+        image: string
+        author: string
+        title: string
+        episode: string
+        season: string
+        summary: string
+    }
 }
